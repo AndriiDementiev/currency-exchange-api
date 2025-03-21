3491e1c74de33d96188ceaf600bab6abaa3e918b
Andrey Dementyev <dementyev.andreyy@gmail.com>
2025-03-21 12:04:52 +0200

    som eadditional fixes

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,24 @@
 FROM python:3.12-slim
 
-# environment variables
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
-
-# directory
-WORKDIR /app
-
-# system dependencies
 RUN apt-get update && apt-get install -y --no-install-recommends \
     libpq-dev curl \
     && rm -rf /var/lib/apt/lists/*
 
-# Poetry
 RUN pip install --no-cache-dir --upgrade pip && pip install --no-cache-dir poetry
 
-# Copy only Poetry files
+WORKDIR /app
+
 COPY pyproject.toml poetry.lock* /app/
 
-# project dependencies using Poetry
-RUN poetry install --no-interaction --no-ansi
+RUN poetry install --no-interaction --no-ansi --no-root
 
 COPY . /app/
 
-# Expose port 8000
+RUN poetry show
+
 EXPOSE 8000
 
-ENTRYPOINT ["poetry", "run", "python", "manage.py"]
-CMD ["runserver", "0.0.0.0:8000"]
+CMD ["poetry", "run", "python", "manage.py", "runserver", "0.0.0.0:8000"]
--- currency_exchange/admin.py
+++ currency_exchange/admin.py
@@ -1,31 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
 from .models import CurrencyExchange, UserBalance
 
-User = get_user_model()
 
-admin.site.unregister(User)
-
-
-@admin.register(User)
-class UserAdmin(DefaultUserAdmin):
-    list_display = ("id", "username", "email", "date_joined", "is_staff")
-    search_fields = ("username", "email")
-    list_filter = ("is_staff", "date_joined")
-    ordering = ("-date_joined",)
-
-
-@admin.register(CurrencyExchange)
-class CurrencyExchangeAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "currency_code", "rate", "created_at")
-    search_fields = ("user__username", "currency_code")
-    list_filter = ("currency_code", "created_at")
-    ordering = ("-created_at",)
-
-
-@admin.register(UserBalance)
-class UserBalanceAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "balance")
-    search_fields = ("user__username",)
-    ordering = ("-balance",)
+admin.site.register(CurrencyExchange)
+admin.site.register(UserBalance)
